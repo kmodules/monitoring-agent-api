cb29a44ae61b9096ce83750c2b070d57daa8d953
Tamal Saha <tamal@appscode.com>
2018-10-12 12:04:48 -0700

    Update kubernetes client libraries to 1.12 (#14)

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -9,11 +9,13 @@ DOCKER_REPO_ROOT="/go/src/$PACKAGE_NAME"
 
 pushd $REPO_ROOT
 
+mkdir -p "$REPO_ROOT"/api/api-rules
+
 # Generate deep copies
 docker run --rm -ti -u $(id -u):$(id -g) \
     -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
     -w "$DOCKER_REPO_ROOT" \
-    appscode/gengo:release-1.11 deepcopy-gen \
+    appscode/gengo:release-1.12 deepcopy-gen \
     --v 1 --logtostderr \
     --go-header-file "hack/gengo/boilerplate.go.txt" \
     --input-dirs "$PACKAGE_NAME/api/v1" \
@@ -23,10 +25,11 @@ docker run --rm -ti -u $(id -u):$(id -g) \
 docker run --rm -ti -u $(id -u):$(id -g) \
     -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
     -w "$DOCKER_REPO_ROOT" \
-    appscode/gengo:release-1.11 openapi-gen \
+    appscode/gengo:release-1.12 openapi-gen \
     --v 1 --logtostderr \
     --go-header-file "hack/gengo/boilerplate.go.txt" \
     --input-dirs "$PACKAGE_NAME/api/v1" \
-    --output-package "$PACKAGE_NAME/api/v1"
+    --output-package "$PACKAGE_NAME/api/v1" \
+    --report-filename api/api-rules/violation_exceptions.list
 
 popd
